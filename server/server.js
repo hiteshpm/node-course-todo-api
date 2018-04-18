1cc557b20b1da480566a2299c8506e3f544ba8d7
Hitesh paliwal <pjhitesh007@gmail.com>
2018-04-18 14:04:55 +0530

    add todo delete

--- server/server.js
+++ server/server.js
@@ -41,6 +41,7 @@ app.get('/todos/:id',(req,res) =>{
    return res.status(400).send();  //return with empty body
 }
 
+
 Todo.findById(id).then((todo) =>{
     if(!todo){
         return res.status(404).send();
@@ -53,6 +54,25 @@ Todo.findById(id).then((todo) =>{
    
 });
 
+//Delete id
+
+app.delete('/todos:id',(req,res) => {
+ var id = req.params.id;
+
+ if(!(ObjectID.isValid)){
+     return res.status(404).send();
+ }
+
+ Todo.findByIdAndRemove(id).then((todo) =>{
+    if(!todo){
+        return res.status(404).send();
+        }
+ 
+    res.send(todo);
+}).catch((e) => {
+    res.status(400).send();
+});
+});
 
 app.listen(port,() => {
    console.log(`Started up at port ${port}`);
